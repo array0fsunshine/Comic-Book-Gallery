ab62c3bb6fc4736478dda9c83c2b724fcebaec84
Emily Rogers <emilysuzannerogers@gmail.com>
2019-01-01 09:52:17 -0800

    Added Action result methods to Detail method

--- Comic Book Gallery/Controllers/ComicBooksController.cs
+++ Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,16 @@ namespace Comic_Book_Gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from the Comic Books Controller!");
+
+        }
     }
 }
